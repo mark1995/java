baf40f4bd30f9418b1b32894f572de529042fdac
mark1995 <504391248@qq.com>
2022-01-04 19:04:08 +0800

    init comet

---
+++ comet/src/main/java/com/jing/CometServer.java
@@ -0,0 +1,23 @@
+package com.jing;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: GXK
+ * @create: 2022-01-04 18:34
+ **/
+
+
+@SpringBootApplication
+public class CometServer {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CometServer.class, args);
+    }
+}
+
+
