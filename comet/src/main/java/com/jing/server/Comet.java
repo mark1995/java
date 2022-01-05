a3dcc2f08b121a8343109a11eedaae87b90a3de2
mark1995 <504391248@qq.com>
2022-01-05 18:08:55 +0800

    add comet code

---
+++ comet/src/main/java/com/jing/server/Comet.java
@@ -0,0 +1,46 @@
+package com.jing.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.net.InetSocketAddress;
+
+/**
+ * @description:
+ * @author: GXK
+ * @create: 2022-01-05 17:02
+ **/
+
+@Component
+public class Comet {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Comet.class);
+
+    private EventLoopGroup boss = new NioEventLoopGroup();
+    private EventLoopGroup worker = new NioEventLoopGroup();
+
+    @Value("${comet.server.port}")
+    private int port;
+
+    @PostConstruct
+    public void startServer() {
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childHandler(new CometInitializer());
+    }
+
+
+}
+
+
